2353e90bef442b4974e818e50321ffd2f5e492f0
Wayne <wabbruscatoorr@gmail.com>
2020-06-05 14:02:38 -0400

    updating files

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from "../App";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  const { getByText } = render(<App />);
+  const header = getByText(/react plant/i);
+  expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  const { getAllByTestId } = render(<CheckoutForm />);
+
+  const success = getAllByTestId(/success message/i);
+
+  fireEvent.change(success, { value: "successMessage" });
+
+  expect(success.value).toBe(/"successMessage"/i);
+
+  fireEvent.click(q(/submit!/i));
+});
